15e081131b0f8a935f912e8d8b155afe25de7085
AlexDeFoc <alex.de.foc@gmail.com>
2024-12-27 22:21:10 +0200

    finished index

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
+/* Buttons */
 
+let work_btn = document.getElementById("work_btn");
+let about_btn = document.getElementById("about_btn");
+let contact_btn = document.getElementById("contact_btn");
+
+function redirect_to_page(page_name)
+{
+    window.location.href = "pages/" + page_name + ".html";
+}
+
+work_btn.addEventListener("click", () => redirect_to_page("work"))
+about_btn.addEventListener("click", () => redirect_to_page("about"))
+contact_btn.addEventListener("click", () => redirect_to_page("contact"))
